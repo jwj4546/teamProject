325b3f873a6386a9efb8e679f1cf2ec734a7522c
joon14 <ehwns1056@naver.com>
2024-05-24 10:23:41 +0900

    update0524

---
+++ teampro01/src/org/ganghwa/dao/MariaDB.java
@@ -0,0 +1,78 @@
+package org.ganghwa.dao;
+
+import java.sql.*;
+
+public class MariaDB implements SqlLang {
+	final static String DRIVER = "org.mariadb.jdbc.Driver";
+	final static String URL = "jdbc:mariadb://192.168.20.221/team72";
+	final static String USERID = "root";
+	final static String USERPW = "1234";
+	final static String INSERT_NOTICE = "insert into notice values(default, ?, ?, default, 0)";
+	final static String LATEST_NOTICE = "select * from notice order by no desc limit 5";
+	final static String LATEST_QNA = "select * from qna order by parno desc, plevel asc limit 5";
+	final static String INSERT_QUESTION = "insert into qna values(default,'1',null,?,?,default,0,?)";
+	final static String INS_ANSWER = "insert into qna values(default,'2',?,?,?,default,0,?)";
+	//public static final String UPDATE_QUESTION_BYPARNO = "update qna set parno=no where no=(select * from qna order by no desc limit 1)";
+	final static String UPDATE_QUESTION_BYPARNO = "update qna as outer_qna join(select no from qna order by no desc limit 1)"
+			+ "as sub_query set outer_qna.parno=sub_query.no where outer_qna.no=sub_query.no";
+			
+	Connection con = null;
+	
+	@Override
+	public Connection connect() {
+		try {
+			Class.forName(DRIVER);
+			try {
+				con = DriverManager.getConnection(URL, USERID, USERPW);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	@Override
+	public void close(Connection con, PreparedStatement pstmt) {
+		if(pstmt!=null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		if(rs!=null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(pstmt!=null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
